f0b034cce3e9d798b4fceb02c32f16e9d6b478ea
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-09-01 15:33:41 -0300

    add dropdown para alterar a listagem das disciplinas

--- app/js/controller/msgsCtrl.js
+++ app/js/controller/msgsCtrl.js
@@ -1,8 +1,17 @@
 angular.module("mural").controller("disciplinasCtrl", function ($scope, disciplinasAPI) {
+
+    let disc = [];
+
     disciplinasAPI.getDisciplinas()
         .then(disciplinas => {
-            $scope.disciplinas = disciplinas.data.sort((a, b) => {
+            // disc = disciplinas.data;
+            disc = disciplinas.data.sort((a, b) => {
                 return a.period - b.period
             });
+            $scope.disciplinas = disc;
         })
+    
+    $scope.setListagem = function (periodo) {
+        $scope.disciplinas = disc.filter(a => a.period === periodo)
+    }
 });
